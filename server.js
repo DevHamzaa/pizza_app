c752bcab9d805c65c1cbac26e575f4790d80f650
DevHamzaa <dev.hamzaa@gmail.com>
2022-03-26 21:08:44 +0500

    nothing

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const eel = require('express-ejs-layouts');
 const router = require('./routes/web');
 const app = express();
-
-app.use(express.json());
 app.use(router);
 
 const port = process.env.PORT || 4000;
