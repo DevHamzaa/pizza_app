2bc86fb19498b6c59de7ce7a052d671f11dfdca5
DevHamzaa <dev.hamzaa@gmail.com>
2022-03-28 19:30:38 +0500

    created Frontend pages

--- routes/web.js
+++ routes/web.js
@@ -3,5 +3,16 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.render('index');
 });
+router.get('/cart', (req, res) => {
+    res.render('./customer/cart');
+});
+router.get('/login', (req, res) => {
+    res.render('./auth/login');
+});
+router.get('/register', (req, res) => {
+    res.render('./auth/register');
+});
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const eel = require('express-ejs-layouts');
 const router = require('./routes/web');
 const app = express();
-app.use(router);
 
 // Set Template Engine
 app.use(express.static(`public`));
 app.use(eel);
 app.set('views', `${__dirname}/resources/views`);
 app.set('view engine', 'ejs');
+app.use(router);
 // Listning to the server
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
